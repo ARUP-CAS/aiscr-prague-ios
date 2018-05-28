608f5a7d09e64ccb2bfceeaf23dd69e57c965bbe
Matej Novak <matej@visualio.cz>
2018-05-28 15:50:35 +0200

    Initial Commit

---
+++ ModernArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModernArchitecture
+//
+//  Created by Matěj Novák on 28.05.18.
+//  Copyright © 2018 Matěj Novák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
