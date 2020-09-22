2a2ea7679244761feb59c5e60b283a1b00d88b22
Matej Novak <matej@visualio.cz>
2020-09-22 09:28:32 +0200

    rm git

--- Pods/Target Support Files/DPPickerManager/DPPickerManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DPPickerManager : NSObject
-@end
-@implementation PodsDummy_DPPickerManager
-@end
--- Pods/Target Support Files/GeoJSONSerialization/GeoJSONSerialization-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GeoJSONSerialization : NSObject
-@end
-@implementation PodsDummy_GeoJSONSerialization
-@end
